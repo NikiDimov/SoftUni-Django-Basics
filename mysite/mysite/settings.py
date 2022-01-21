8fecbd395a674dcc232e8e0bb684bb12196ed8b8
niki <niki.dimov86@gmail.com>
2022-01-22 00:05:15 +0200

    django introduction 21.01.22

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mysite',
         'USER': 'postgres',
-        'PASSWORD': 'nodnod',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': '5433',
     }
